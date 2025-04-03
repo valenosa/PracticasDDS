222ffe47c34013aae8b4c3dd236c99e32ff5bbdd
osavalentin <83839175+valenosa@users.noreply.github.com>
2025-04-03 16:42:11 -0300

    Tests unitarios para la clase Inscripcion

---
+++ src/test/java/domain/entidades/InscripcionTest.java
@@ -0,0 +1,54 @@
+package domain.entidades;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InscripcionTest {
+
+
+
+    @Test
+    @DisplayName("Un alumno con sypn,pdep,ingles1,ayed y ads puede inscribirse a dds")
+    public void InscripcionAprobada(){
+        Materia sypn = new Materia();
+        Materia pdep = new Materia();
+        Materia ingles1 = new Materia();
+        Materia ayed = new Materia();
+        Materia ads = new Materia();
+        Materia dds = new Materia();
+        dds.agregarCorrelativas(sypn,pdep,ingles1,ayed,ads);
+
+        Alumno alumno = new Alumno();
+        alumno.agregarAprobadas(sypn,pdep,ingles1,ayed,ads);
+
+        Inscripcion inscripcion = new Inscripcion(alumno);
+        inscripcion.agregarMaterias(dds);
+
+        Assertions.assertTrue(inscripcion.aprobada());
+    }
+
+    @Test
+    @DisplayName("Un alumno sin sypn tiene su inscripción rechazada")
+    public void InscripcionRechazada(){
+        Materia sypn = new Materia();
+        Materia pdep = new Materia();
+        Materia ingles1 = new Materia();
+        Materia ayed = new Materia();
+        Materia ads = new Materia();
+        Materia dds = new Materia();
+        dds.agregarCorrelativas(sypn,pdep,ingles1,ayed,ads);
+
+        Alumno alumno = new Alumno();
+        alumno.agregarAprobadas(pdep,ingles1,ayed,ads);
+
+        Inscripcion inscripcion = new Inscripcion(alumno);
+        inscripcion.agregarMaterias(dds);
+
+        Assertions.assertFalse(inscripcion.aprobada());
+    }
+
+
+}
